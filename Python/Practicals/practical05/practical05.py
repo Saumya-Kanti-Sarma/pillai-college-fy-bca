2f096fa45c6a08f8a79926e425e2aa1581aad799
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-11-01 07:25:20 +0530

    updated practical05

--- Python/Practicals/practical05/practical05.py
+++ Python/Practicals/practical05/practical05.py
@@ -8,23 +8,27 @@ Build a phonebook application.
 ● Display all contacts in alphabetical order of names.
 """
 contact = {
-    "Tvisha":987654320,
+    "Abi":987654320,
     "Roshan":789456120,
     "Saumya":12457890
 }
+print("All contacts:",contact)
 
-name = input("Enter name : ")
+# Add new contacts to the dictionary
+print("Add to Contact")
+name = input("Enter Name : ")
 ph = int(input("Enter number : "))
 contact.update({name:ph})
 print(contact)
 
-inp = input("Enter name to search: ")
+# Search for a contact by name
+inp = input("Search Contact: ")
 data = contact.get(inp)
 print(inp, ":",data)
 
-name = input("Enter name : ")
+# Update an existing contact’s number. 
+name = input("Update Contact: ")
 is_available = contact.get(name)
-
 if is_available == False:
     print("No contact available")
 else:
@@ -32,8 +36,8 @@ else:
     contact.update({name:ph})
     print(contact)
 
-
-name = input("Enter name : ")
+#Delete a contact. 
+name = input("Delete Contact: ")
 is_available = contact.get(name)
 if is_available== None:
     print("No contact available")
@@ -41,48 +45,6 @@ else:
     del contact[name]
     print(contact)
 
-while True:
-    task = input("Enter 'a' to add new contact, 's' to search, 'd' to delete and 'c' to show all contact: ")
-    task.lower()
-    if task == "a":
-        #Add new contacts to the dictionary. 
-        name = input("Enter name : ")
-        ph = int(input("Enter number : "))
-        contact.update({name:ph})
-        print(contact)
-    elif task == "s":
-        #Search for a contact by name. 
-        inp = input("Enter name to search: ")
-        data = contact.get(inp)
-        print(inp, ":",data)
-    elif task == "u":
-        #Update an existing contact’s number.
-        name = input("Enter name : ")
-        is_available = contact.get(name)
-
-        if is_available == None:
-            print("No contact available")
-        else:
-            ph = int(input("Enter number : "))
-            contact.update({name:ph})
-            print(contact)
-    elif task == "d":
-        #Delete a contact.
-        name = input("Enter name : ")
-        is_available = contact.get(name)
-
-        if is_available== None:
-            print("No contact available")
-        else:
-            del contact[name]
-            print(contact)
-    elif task == "c":
-        if contact:
-            print("All contacts (sorted by name):")
-            for name in sorted(contact.keys()):
-                print(f"{name}: {contact[name]}")
-        else:
-            print("No contacts available")
-    elif task =="break":
-        break
-
+# print dictionary in sorted order:
+for name in sorted(contact.keys()):
+    print(f"{name}: {contact[name]}")
